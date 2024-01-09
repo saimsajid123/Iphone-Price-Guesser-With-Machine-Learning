fb74e2e87ddecb1a214d8c32b1e2c29586244c62
Muhammad Saim Sajid <141317850+saimsajid123@users.noreply.github.com>
2024-01-10 00:24:17 +0500

    Update Iphonepriceguesser.py

--- Iphonepriceguesser.py
+++ Iphonepriceguesser.py
@@ -3,22 +3,30 @@ from tkinter import ttk
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-
 class PriceGuesser:
     def __init__(self):
         self.root = tk.Tk()
-        self.entry1 = tk.Entry(self.root)
-        self.entry1.pack()
-        self.button1 = tk.Button(self.root, text="Predict Price", command=self.predict_price)
-        self.button1.pack()
+        self.root.title("Price Guesser")
 
-        self.data = pd.read_csv("pricegusserofiphone.csv")
+        style = ttk.Style()
+        style.configure("TButton", padding=5, font=('Helvetica', 12))
+
+        self.entry_label = ttk.Label(self.root, text="Enter iPhone Version:")
+        self.entry_label.pack(pady=10)
+
+        self.entry1 = ttk.Entry(self.root)
+        self.entry1.pack(pady=10)
+
+        self.button1 = ttk.Button(self.root, text="Predict Price", command=self.predict_price)
+        self.button1.pack(pady=10)
+
+        self.data = pd.read_csv("priceguesserofiphone.csv")
 
         self.model = LinearRegression()
         self.model.fit(self.data[["version"]], self.data["price"])
 
-        self.label_result = tk.Label(self.root, text="")
-        self.label_result.pack()
+        self.label_result = ttk.Label(self.root, text="")
+        self.label_result.pack(pady=10)
 
         self.root.mainloop()
 
@@ -33,6 +41,5 @@ class PriceGuesser:
             result_text = "Please enter a valid numeric value."
 
         self.label_result.config(text=result_text)
-
 
 app = PriceGuesser()
